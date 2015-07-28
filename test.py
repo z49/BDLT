763809d707d5b1d488e001f48d487cb5f23d41ff
Broderick Gardner <--global>
2015-07-28 17:47:17 -0600

    Read config out, write config to, and change config  manually all work

--- test.py
+++ test.py
@@ -1,23 +1,48 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import time
 import serial
 import sys
-import binascii as b
 import struct
+import collections as cs
+import binascii as b
+import copy
+import readline
+import ast
 
-class BinaryReaderEOFException(Exception):
-    def __init__(self):
-        pass
+def rlinput(prompt, prefill=''):
+   readline.set_startup_hook(lambda: readline.insert_text(prefill))
+   try:
+      return input(prompt)
+   finally:
+      readline.set_startup_hook()
+
+class Config:
+    CONFIGSIZE = 44
+    fmt = 'Hh8h11sxbb5s5s'
+    st = struct.Struct('Hh8h11sxbb5s5s')
+    #keys = ['ints','pulse','A','G','B','L','M','S','V','na','file','mode','channel','txaddr','rxaddr']
+    keys = ['flags','pulse','accel','gyro','baro','light','magneto','sync','voltage','/na','file','mode','channel','txaddr','rxaddr']
+
+    def __init__(self,data):
+        values = Config.st.unpack(data)
+        self.cfg = cs.OrderedDict(zip(Config.keys,values))
+
     def __str__(self):
-        return 'Not enough bytes to satisfy read request'
+        pdict = copy.deepcopy(self.cfg)
+        #pdict['txaddr'] = b.hexlify(pdict['txaddr'])
+        #pdict['rxaddr'] = b.hexlify(pdict['rxaddr'])
+        #pdict['flags'] = bin(pdict['flags'])[2:].zfill(8)
+        #pdict['mode'] = bin(pdict['mode'])[2:].zfill(8)
+        string = ''
+        for k,v in pdict.items():
+            string += k + '\t: ' + str(v) + '\n'
+        return string
 
-def read_data(data):
-    fmt = 'H h 8h 11s x b b 5s 5s'
-    size = struct.calcsize(fmt)
-    #if size > len(data):
-    #    raise BinaryReaderEOFException
-    return struct.unpack(fmt,data)
+    def repack(self):
+        return Config.st.pack(*self.cfg.values())
+
+
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
@@ -39,18 +64,23 @@ while 1 :
 #    input = raw_input(">> ")
         # Python 3 users
     inp = input(">> ")
+
     if inp == 'x':
         ser.close()
         exit()
+
+#change read config
     elif inp == 'change':
-        print('Changing struct...')
-        data[11] = 1
-        data[0] = 2
-        print(data)
+        newinp = rlinput('edit config:\n',str(ldcfg))
+        for line in newinp.splitlines():
+            ldcfg.cfg[line[:line.find('\t')]] = ast.literal_eval(line[line.index(':')+2:])
+
+#Write config back to device
     elif inp == 'write':
-        ser.write('wt\r'.encode('ascii'));
-        ser.write(struct.pack('H h 8h 11s x b b 5s 5s',*data))
-        data2 = list(read_data(ser.read(44)))
+        data = ldcfg.repack()
+        ser.write('wt\r'.encode('ascii'))
+        ser.write(data)
+        data2 = ser.read(Config.CONFIGSIZE)
         if data2 == data:
             print('correct')
             ser.write(bytes([255]))
@@ -59,44 +89,39 @@ while 1 :
             print(data2)
             ser.write(bytes([0]))
         print(ser.readline().decode(),end='')
+
+#Read config from device
+    elif inp == 'read':
+        ser.write('rd\r'.encode('ascii'))
+        out = ''
+        i = 10
+        while ser.inWaiting() == 0 and i > 0:
+            i = i - 1
+            time.sleep(.1)
+        while(out != 'read\n#!bin\n'):
+            out += ser.read(1).decode('ascii')
+        byts = ser.read(Config.CONFIGSIZE)
+        while(ser.inWaiting()):
+            ser.read(1)
+        ldcfg = Config(byts)
+        print(ldcfg)
+
+#Send command straight to device.  Will later be deprecated, 
+#as all will be handled by client
     else:
-        # send the character to the device
-        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
-        inp += '\r\n'
+        inp += '\r'
         ser.write(inp.encode('ascii'))
         out = ''
-        byts = b.hexlify(bytes([]))
         # let's wait one second before reading output (let's give device time to answer)
         #time.sleep(1)
-        binbytes = 0
         i = 10
         while ser.inWaiting() == 0 and i > 0:
             i = i - 1
             time.sleep(.1)
         while ser.inWaiting() > 0:
-            #print('out: ',end='')
-            #print(out.encode())
-            #print('bin: ',end='')
-            #print(binbytes)
-            byt = ser.read(1)
-            if out == 'read\n#!bin\n':
-                binbytes = 44
-                out = 'binary:'
-            try:
-                if binbytes > 0:
-                    binbytes = binbytes - 1
-                    byts += b.hexlify(byt)
-                else:
-                    out += byt.decode('ascii')
-            except:
-                print('ER')
-                #print(binbytes)
-                #print(byt)
-                #print(byts)
-
+            while ser.inWaiting() > 0:
+                byt = ser.read(1)
+                out += byt.decode('ascii')
+            time.sleep(.1)
         if out != '':
             print(out,end='')
-        if byts != b.hexlify(bytes([])):
-            data = read_data(b.unhexlify(byts))
-            data = list(data)
-            print(data)
